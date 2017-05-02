2373f281230e0985c8f9a32d7277f1ca18b8e453
Michael Davidson <realityenigma@hotmail.com>
2017-05-01 22:07:51 -0500

    Adding stirng params to logger interface to allow for greater logging framework integration flexibility.

--- src/OrientDB.Net.Core/Abstractions/IOrientDBLogger.cs
+++ src/OrientDB.Net.Core/Abstractions/IOrientDBLogger.cs
@@ -2,11 +2,11 @@
 {
     public interface IOrientDBLogger
     {
-        void Debug(string message);
-        void Information(string message);
-        void Verbose(string message);
-        void Error(string message);
-        void Fatal(string message);
-        void Warning(string message);
+        void Debug(string message, params string[] properties);
+        void Information(string message, params string[] properties);
+        void Verbose(string message, params string[] properties);
+        void Error(string message, params string[] properties);
+        void Fatal(string message, params string[] properties);
+        void Warning(string message, params string[] properties);
     }
 }
