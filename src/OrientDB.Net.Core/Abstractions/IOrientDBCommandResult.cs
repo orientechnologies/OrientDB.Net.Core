96d223b817e6fb6f3bd93cfab8336bfd2eb058fa
Michael Davidson <realityenigma@hotmail.com>
2017-05-11 21:19:34 -0500

    Updated CommandResult interface to handle command record results.

--- src/OrientDB.Net.Core/Abstractions/IOrientDBCommandResult.cs
+++ src/OrientDB.Net.Core/Abstractions/IOrientDBCommandResult.cs
@@ -1,7 +1,12 @@
-﻿namespace OrientDB.Net.Core.Abstractions
+﻿using OrientDB.Net.Core.Models;
+using System.Collections.Generic;
+
+namespace OrientDB.Net.Core.Abstractions
 {
     public interface IOrientDBCommandResult
     {
         int RecordsAffected { get; }
+
+        IEnumerable<DictionaryOrientDBEntity> UpdatedRecords { get; }
     }
 }
